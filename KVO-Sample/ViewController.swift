837eccf90cdce17555b28a893c79f895aa29862a
Tri Ngo <ngominhtrint@gmail.com>
2018-11-27 22:04:16 +0800

    Initial Commit

---
+++ KVO-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KVO-Sample
+//
+//  Created by TriNgo on 11/27/18.
+//  Copyright © 2018 RoverDream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
